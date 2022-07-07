26da30dfe3aeea0be59bda0449b65f7d6c5ff1bc
Alana Sampaio <71518539+AlanaSampaio@users.noreply.github.com>
2022-07-07 08:27:41 -0300

    implementação do findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,14 +10,8 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1, "Books");
-		
-		System.out.println(obj);
-		System.out.println("-*-*-*-*-*-*-*-*-*");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3.000, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
