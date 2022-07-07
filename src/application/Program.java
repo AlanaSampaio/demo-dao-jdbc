cb0684ee8ac5c2f9a885c1373e10a49272f88039
Alana Sampaio <71518539+AlanaSampaio@users.noreply.github.com>
2022-07-07 11:53:45 -0300

    implementação do update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Gred", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserido! Novo id: " + newSeller.getId());
+		
+		System.out.println("\n==TESTE5: seller update==");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Atualização completa!");
 	}
 
 }
